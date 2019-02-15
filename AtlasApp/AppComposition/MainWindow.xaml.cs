9639c557f7f2ce3e819ef7d92db0b46aed6c0e1a
Kevin Bindig <kbindig98@web.de>
2019-02-15 15:05:08 +0100

    finished the crusade against code behind (deleted unneccessary code)

--- AtlasApp/AppComposition/MainWindow.xaml.cs
+++ AtlasApp/AppComposition/MainWindow.xaml.cs
@@ -8,47 +8,12 @@ using System.Windows.Navigation;
 
 namespace AtlasApp.AppComposition
 {
-    public partial class MainWindow : INotifyPropertyChanged
+    public partial class MainWindow
     {
-        private bool _hasValidURI;
-        public bool HasValidURI
-        {
-            get { return _hasValidURI; }
-            set { _hasValidURI = value; OnPropertyChanged("HasValidURI"); }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public void OnPropertyChanged(string name)
-        {
-            var handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(name));
-        }
-
         public MainWindow(MainWindowViewModel mainWindowViewModel)
         {
             DataContext = mainWindowViewModel;
             InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            Uri uri;
-            HasValidURI = Uri.TryCreate((sender as TextBox).Text, UriKind.Absolute, out uri);
-        }
-
-        private void TextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            Uri uri;
-            if (Uri.TryCreate((sender as TextBox).Text, UriKind.Absolute, out uri))
-            {
-                Process.Start(new ProcessStartInfo(uri.AbsoluteUri));
-            }
-        }
-        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
-            e.Handled = true;
         }
     }
 }
